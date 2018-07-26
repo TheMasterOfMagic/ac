bcb103817e14a18892fe7f373a2b6b85825a2336
钟总 <18811569079@163.com>
2018-07-26 20:30:07 +0800

    修复了保存文件时的一些Bug，同时添加了删除文件的逻辑

--- models/file.py
+++ models/file.py
@@ -1,23 +1,40 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, and_
+from os import remove, path
 from database import db
+from config import storage_path
 
 
 class File(db.Model):
     __tablename__ = 'files'
     creator_id = Column(Integer, ForeignKey('users.id_', ondelete='CASCADE'), primary_key=True, autoincrement=True)
     filename = Column(String(64), primary_key=True)
-    length = Column(Integer, primary_key=True)
     hash_value = Column(String(128))
 
     @classmethod
-    def upload_file(cls, user, filename, content):
-        from config import storage_path
+    def upload_file(cls, user, data):
         from hashlib import sha512
+        filename = data.filename
+        assert len(filename) <= 64, 'filename too long (>64B)'
+        f = File.query.filter(and_(File.creator_id == user.id_, File.filename == filename)).first()
+        assert not f, 'file already exists'
+        content = data.read()
+        assert len(content) < 1*1024*1024, 'file too large (>=10MB)'
         creator_id = user.id_
-        length = len(content)
         hash_value = sha512(content).hexdigest()
-        with open(storage_path+'hash_value', 'wb') as f:
-            f.write(content)
-        file = File(creator_id=creator_id, filename=filename, length=length, hash_value=hash_value)
+        if not path.exists(storage_path+hash_value):
+            with open(storage_path+hash_value, 'wb') as f:
+                f.write(content)
+        file = File(creator_id=creator_id, filename=filename, hash_value=hash_value)
         db.session.add(file)
         db.session.commit()
+
+    @classmethod
+    def delete_file(cls, user, filename):
+        f = File.query.filter(File.creator_id == user.id_ and File.filename == filename).first()
+        assert f, 'no such file ({})'.format(filename)
+        hash_value = f.hash_value
+        db.session.delete(f)
+        db.session.commit()
+        files = File.query.filter(File.hash_value == hash_value).all()
+        if not len(files):
+            remove(storage_path+hash_value)
--- views/file.py
+++ views/file.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, flash, redirect
+from flask import Blueprint, render_template, flash, redirect, request
+from models import File
 from common import *
 
 file = Blueprint('file', __name__)
@@ -9,8 +10,7 @@ file = Blueprint('file', __name__)
 def get__file(user):
     from models import File
     files = File.query.filter(File.creator_id == user.id_).all()
-    filenames = list(f.filename for f in files)
-    return render_template('file.html', username=user.username, filenames=filenames)
+    return render_template('file.html', username=user.username, files=files)
 
 
 @file.route('/upload')
@@ -25,18 +25,26 @@ def get__upload():
 def post__upload(user):
     try:
         from form import FileForm
-        from models import File
         form = FileForm()
         assert form.validate_on_submit(), 'invalid form fields'
         data = form.file.data
-        filename, content_length = data.filename, data.content_length
-        assert len(filename) <= 64, 'filename too long (>64B)'
-        content = form.file.data.read()
-        assert not File.query.filter(File.creator_id == user.id_ and File.filename == filename), 'file already exists'
-        assert len(content) < 1*1024*1024, 'file too large (>=10MB)'
-        File.upload_file(user, filename, content)
+        File.upload_file(user, data)
         flash('上传成功！')
     except AssertionError as e:
         message = e.args[0] if len(e.args) else str(e)
         flash('上传失败！'+message)
+    return redirect('/file')
+
+
+@file.route('/remove')
+@login_required
+def get__remove(user):
+    try:
+        filename = request.args.get('filename')
+        assert filename, 'missing filename'
+        File.delete_file(user, filename)
+        flash('删除成功！')
+    except AssertionError as e:
+        message = e.args[0] if len(e.args) else str(e)
+        flash('删除失败！'+message)
     return redirect('/file')
